fcf55ed3d618cd93fb1320bb14bf9c2478d6156e
psychoria <psychoria@hotmail.com>
2017-07-25 23:50:24 +0900

    DHT-11 sample for C#

---
+++ DHT11sample/Dht11.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace TempHumidReader
+{
+    public class Dht11Reading
+    {
+        private const int BIT_SIZE = 40;
+        public BitArray bits;
+
+        public Dht11Reading()
+        {
+            bits = new BitArray(BIT_SIZE);
+        }
+        public bool IsValid()
+        {
+            long value = BitArrayToLong();
+            long checksum = ((value >> 32) & 0xff) + ((value >> 24) & 0xff)
+                + ((value >> 16) & 0xff) + ((value >> 8) & 0xff);
+
+            return (checksum & 0xff) == (value & 0xff);
+        }
+
+        public double Humidity()
+        {
+
+            long value = BitArrayToLong();
+            return ((value >> 32) & 0xff) + ((value >> 24) & 0xff) / 10.0;
+        }
+
+        public double Temperature()
+        {
+            long value = BitArrayToLong();
+            return ((value >> 16) & 0xff) + ((value >> 8) & 0xff) / 10.0;
+        }
+
+        private long BitArrayToLong()
+        {
+            long value = 0;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i])
+                    value += Convert.ToInt64(Math.Pow(2, i));
+            }
+            return value;
+        }
+    }
+
+    public class Dht11Sensor
+    {
+        private const int SAMPLE_HOLD_LOW_MILLIS = 18;
+        private GpioPinDriveMode inputDriveMode;
+        private GpioPin pin;
+
+        public int Init(int pinNumber)
+        {
+            var gpio = GpioController.GetDefault();
+            if (null == gpio)
+            {
+                return 1;
+            }
+            try
+            {
+                pin = gpio.OpenPin(pinNumber);
+            }
+            catch
+            {
+                return 2;
+            }
+
+            inputDriveMode = pin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp) ? 
+                GpioPinDriveMode.InputPullUp : GpioPinDriveMode.Input;
+            pin.SetDriveMode(inputDriveMode);
+
+            return 0;
+        }
+
+        private int InternalSample(Dht11Reading Reading)
+        {
+            // This is the threshold used to determine whether a bit is a '0' or a '1'.
+            // A '0' has a pulse time of 76 microseconds, while a '1' has a
+            // pulse time of 120 microseconds. 110 is chosen as a reasonable threshold.
+            long oneThreshold = 110L * System.Diagnostics.Stopwatch.Frequency / 1000000L; // 110microsec
+
+            // Latch low value onto pin
+            pin.Write(GpioPinValue.Low);
+
+            // Set pin as output
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            // Wait for at least 18 ms
+            Task.Delay(18).Wait();
+
+            // Set pin back to input
+            pin.SetDriveMode(inputDriveMode);
+
+            GpioPinValue previousValue = pin.Read();
+
+            // catch the first rising edge
+            long initialRisingEdgeTimeoutMillis = 10000L; // 1ms
+            long endTickCount = DateTime.Now.Ticks + initialRisingEdgeTimeoutMillis;
+            for (;;)
+            {
+                if (DateTime.Now.Ticks > endTickCount)
+                {
+                    return 1;
+                }
+
+                GpioPinValue value = pin.Read();
+                if (value != previousValue)
+                {
+                    // rising edge?
+                    if (value == GpioPinValue.High)
+                    {
+                        break;
+                    }
+                    previousValue = value;
+                }
+            }
+
+            long prevTime = 0;
+
+            long sampleTimeoutMillis = 100000L; // 10ms
+            endTickCount = DateTime.Now.Ticks + sampleTimeoutMillis;
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            // capture every falling edge until all bits are received or
+            // timeout occurs
+            for (int i = 0; i < Reading.bits.Length + 1;)
+            {
+                if (DateTime.Now.Ticks > endTickCount)
+                    return 2;
+
+                GpioPinValue value = pin.Read();
+                if ((previousValue == GpioPinValue.High) && (value == GpioPinValue.Low))
+                {
+                    long now = sw.ElapsedTicks;
+
+                    if (i != 0)
+                    {
+                        long difference = now - prevTime;
+                        Reading.bits[Reading.bits.Length - i] = difference > oneThreshold;
+                    }
+                    prevTime = now;
+                    ++i;
+                }
+                previousValue = value;
+            }
+
+            if (!Reading.IsValid())
+            {
+                // checksum mismatch
+                return 3;
+            }
+            return 0;
+        }
+
+        public int Sample(out Dht11Reading Reading)
+        {
+            Reading = new Dht11Reading();
+            int result = 0;
+            int retryCount = 0;
+            do
+            {
+                result = InternalSample(Reading);
+            } while (0 != result && (++retryCount < 20));
+
+            return result;
+        }
+    }
+}
---
+++ DHT11sample/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.System.Threading;
+using Windows.Devices.Gpio;
+using TempHumidReader;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DHT11sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Dht11Sensor dht11;
+        public static int errorCount1 = 0, errorCount2 = 0, errorCount3 = 0;
+        private const int PIN_NUM = 4;
+        private DispatcherTimer timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            dht11 = new Dht11Sensor();
+            if (0 != dht11.Init(PIN_NUM))
+            {
+                statusText.Text = "Failed to open GPIO pin";
+                return;
+            }
+            statusText.Text = "Init Success!!";
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(4000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        public void Timer_Tick(object sender, object e)
+        {
+            Dht11Reading reading;
+            int result = dht11.Sample(out reading);
+
+            if (0 != result)
+            {
+                if (1 == result)
+                    ++errorCount1;
+                else if (2 == result)
+                    ++errorCount2;
+                else
+                    ++errorCount3;
+
+                statusText.Text = String.Format("#1 Error : {0:d}, #2 Error : {1:d}, #3 Error : {2:d}", errorCount1, errorCount2, errorCount3);
+            }
+            else
+            {
+                humidityText.Text = String.Format(("Humidity : {0:f1}"), reading.Humidity());
+                temperatureText.Text = String.Format("Temperature : {0:f1}", reading.Temperature());
+            }
+        }
+    }
+}
